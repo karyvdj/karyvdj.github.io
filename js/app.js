cbfd489f5183596a5a624decc78f9521d6adb423
karyvdj <kaina51@hotmail.com>
2017-07-31 11:12:50 -0500

    Agregando textos y areglando detalles en proyectos

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-var cargarPagina = function(){
-    navCambio();
-}
-
-function navCambio() {
-    var nav = $("#nav");
-    if (window.scrollY > 50) {
-        nav.removeClass("opacidad");
-
-    } else if (window.scrollY > 400) {
-        nav.addClass("opacidad");
-    }
-}
-
-document.addEventListener("scroll", navCambio);
-
-$(document).ready(cargarPagina);
+// var cargarPagina = function(){
+//     navCambio();
+// }
+//
+// function navCambio() {
+//     var nav = $("#nav");
+//     if (window.scrollY > 50) {
+//         nav.removeClass("opacidad");
+//
+//     } else if (window.scrollY > 400) {
+//         nav.addClass("opacidad");
+//     }
+// }
+//
+// document.addEventListener("scroll", navCambio);
+//
+// $(document).ready(cargarPagina);
