e72b21b5285135c884a23f3ddcfece533fef2d41
karyvdj <kaia51@hotmail.com>
2017-08-14 00:07:55 -0500

    añadiendo splash

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   setTimeout(function() {
+      $('#splash').hide(700);
+      $('#fondo-portada').show(700);
+      $('body').removeClass('no-scroll');
+      $('#menu').removeClass("ocultar");
+      $('#menu').addClass("visible");
+   }, 3000);
+})
